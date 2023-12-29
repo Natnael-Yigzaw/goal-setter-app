ec5062d9d51fc1f871c60ea72afaeb1ca74c1596
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2023-12-29 14:30:53 +0000

    Update getProfile to return the user object from the request instead of querying the database.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,17 +78,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const getProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
+  res.status(200).json(req.user);
 });
 
 module.exports = { registerUser, loginUser, logoutUser, getProfile };
