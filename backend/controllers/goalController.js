82137cb42beba5b60860f99fbaebb00d70a593de
Natnael_Yigzaw <127339716+Natnael-Yigzaw@users.noreply.github.com>
2023-12-29 14:36:13 +0000

    Update and delete goal routes to use req.user instead of req.user.id

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -28,14 +28,12 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found.");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -54,14 +52,12 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found.");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
